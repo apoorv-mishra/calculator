07265f5734cca6bdb69e754de4269d4abe3b8880
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-01-27 12:52:16 +0530

    Add calculator UI

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+function renderKeypad() {
+  const rows = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+']
+  ];
+
+  const rowsToRender = []
+  rows.forEach((row, rowIndex) => {
+    const keysToRender = [];
+    row.forEach((key, keyIndex) => {
+      keysToRender.push(
+       <Key key={`key-${keyIndex}`} value={key} />
+      );
+    });
+    rowsToRender.push(
+      <View style={styles.keypadRow} key={`row-${rowIndex}`}>{keysToRender}</View>
+    );
+  });
+  return rowsToRender;
+}
+
+const Key = (props) => {
+  return (
+    <View style={styles.key}>
+      <Text style={styles.keyText}>{props.value}</Text>
+    </View>
+  );
+};
+
+const Keypad = () => {
+  return (
+    <View style={styles.keypad}>{renderKeypad()}</View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>A basic calculator!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.calculator}>
+      <View style={styles.screen}></View>
+      <Keypad />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  calculator: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  screen: {
+    flex: 3,
+    backgroundColor: 'rgb(25,51,66)'
+  },
+  keypad: {
+    flex: 7,
+    backgroundColor: 'rgb(61,96,111)',
+    flexDirection: 'column'
+  },
+  keypadRow: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  key: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 0.5,
+    borderColor: '#91AA9D',
+  },
+  keyText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: 'white'
+  }
 });
