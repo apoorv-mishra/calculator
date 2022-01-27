1a5f4f07221a7340829371579de9f26fdade6efc
Apoorv Mishra <apoorvmishra@saeloun.com>
2022-01-27 19:06:58 +0530

    Add state

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
-function renderKeypad() {
+function renderKeypad(props) {
   const rows = [
     [1, 2, 3, '/'],
     [4, 5, 6, '*'],
     [7, 8, 9, '-'],
-    [0, '.', '=', '+']
+    [0, 'C', '=', '+']
   ];
 
   const rowsToRender = []
@@ -13,7 +14,7 @@ function renderKeypad() {
     const keysToRender = [];
     row.forEach((key, keyIndex) => {
       keysToRender.push(
-       <Key key={`key-${keyIndex}`} value={key} />
+        <Key key={`key-${keyIndex}`} value={key} onKeyPress={props.onKeyPress}/>
       );
     });
     rowsToRender.push(
@@ -25,23 +26,56 @@ function renderKeypad() {
 
 const Key = (props) => {
   return (
-    <View style={styles.key}>
+    <TouchableHighlight
+      style={styles.key}
+      underlayColor="#193441"
+      onPress={() => props.onKeyPress(props.value)}
+    >
       <Text style={styles.keyText}>{props.value}</Text>
-    </View>
+    </TouchableHighlight>
   );
 };
 
-const Keypad = () => {
+const Keypad = (props) => {
   return (
-    <View style={styles.keypad}>{renderKeypad()}</View>
+    <View style={styles.keypad}>{renderKeypad(props)}</View>
   );
 };
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [operator, setOperator] = useState("");
+  const [prevInput, setPrevInput] = useState("");
+  const [isInputResult, setIsInputResult] = useState(false);
+  const onKeyPress = (key) => {
+    if (isInputResult && key !== 'C') {
+      return alert("Press C!")
+    }
+    if (typeof key === 'number') {
+      setInput(input + key);
+    } else if (key ===  '/' || key === '*' || key === '-' || key === '+') {
+      if (!operator) {
+        setOperator(key);
+        setPrevInput(input);
+        setInput("")
+      }
+    } else if (key === '=') {
+      setInput(eval(prevInput + operator + input));
+      setIsInputResult(true);
+    } else {
+      setPrevInput("");
+      setInput("");
+      setOperator("");
+      setIsInputResult(false);
+    }
+  };
+
   return (
     <View style={styles.calculator}>
-      <View style={styles.screen}></View>
-      <Keypad />
+      <View style={styles.display}>
+        <Text style={styles.displayText}>{input}</Text>
+      </View>
+      <Keypad onKeyPress={onKeyPress}/>
     </View>
   );
 }
@@ -50,9 +84,17 @@ const styles = StyleSheet.create({
   calculator: {
     flex: 1,
   },
-  screen: {
+  display: {
     flex: 3,
-    backgroundColor: 'rgb(25,51,66)'
+    backgroundColor: 'rgb(25,51,66)',
+    justifyContent: 'center',
+  },
+  displayText: {
+    color: 'white',
+    fontSize: 38,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    padding: 20
   },
   keypad: {
     flex: 7,
